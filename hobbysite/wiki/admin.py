8cae3f9b1844595d91d4a592ec11c24898d1dcfa
jamesmacasaet <james.emmanuelle.m@gmail.com>
2024-03-18 16:31:38 +0800

    admin update

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from .models import Article, ArticleCategory
 
-admin.site.register(Article)
-admin.site.register(ArticleCategory)
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+
+    search_fields=['name']
+    list_display=['name']
+
+    fieldsets =[("Details", {"fields:" ("name", "description")})]
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+
+    search_fields = [
+        "title",
+    ]
+    list_display = ["title", "category", "created_on", "updated_on"]
+    list_filter = ["created_on", "updated_on",]
+
+    fieldsets =[("Details", {"fields:" [("title", "entry"), "category"})]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+
+
+task_list.html
+{% for task in objec_list}
+<li>
+    <a href="{{ task.get_absolute_url }}">
