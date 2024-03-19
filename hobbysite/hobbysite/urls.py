fcb16688ea53597b4aa0ba2b50cef9f6769d9b51
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-03-19 15:34:44 +0800

    Fixed wiki urls

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns=[
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls', namespace="forum")),
-    path('wiki/', include('wiki.urls'))
+    path('wiki/', include('wiki.urls', namespace="wiki"))
 ]
