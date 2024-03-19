c301282886d1d40cd3ccce155a03a6f943d07624
kiseshka <146538810+kiseshka@users.noreply.github.com>
2024-03-20 00:22:03 +0800

    Add Commission App

---
+++ hobbysite/commissions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    commission = models.ForeignKey(Commission, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.id, self.commission.title)
---
+++ hobbysite/commissions/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('list', views.commission_list, name='commission_list'),
+    path('detail/<int:pk>', views.commission_detail, name='commission_detail'),
+]
