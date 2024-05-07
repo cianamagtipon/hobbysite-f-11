5e4133c85d41ee0174039e4dda9e0944a7d21fa1
ClydeGerance <clyde.gerance@student.ateneo.edu>
2024-05-07 17:48:02 +0800

    added views and models

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=63)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.display_name
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -9,10 +17,8 @@ class ProductType(models.Model):
     def __str__(self):
         return self.name
     
-    
     class Meta:
         ordering = ['name']
-
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -20,9 +26,22 @@ class Product(models.Model):
         ProductType, 
         on_delete=models.SET_NULL, 
         null=True, 
-        related_name='products')
+        related_name='products'
+    )
+    owner = models.ForeignKey(
+        Profile, 
+        on_delete=models.CASCADE,
+        null=True 
+    )
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('On sale', 'On sale'),
+        ('Out of stock', 'Out of stock')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')
 
     def __str__(self):
         return self.name
@@ -30,6 +49,22 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('merchstore:detail', args=[self.pk])
     
-    
     class Meta:
         ordering = ['name']
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    amount = models.IntegerField()
+    STATUS_CHOICES = [
+        ('On cart', 'On cart'),
+        ('To Pay', 'To Pay'),
+        ('To Ship', 'To Ship'),
+        ('To Receive', 'To Receive'),
+        ('Delivered', 'Delivered')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='On cart')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.buyer.display_name}"
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Product
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Product, Transaction
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class ProductTypeView(ListView):
     model = Product
@@ -21,3 +23,39 @@ class ProductDetailView(DetailView):
         related_products = self.object.product_type.products.all()
         context['related_products'] = related_products
         return context
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    fields = ['name', 'product_type', 'description', 'price', 'stock', 'status']
+    
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    fields = ['name', 'product_type', 'description', 'price', 'stock']
+    
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        if form.instance.stock == 0:
+            form.instance.status = 'Out of stock'
+        else:
+            form.instance.status = 'Available'
+        return super().form_valid(form)
+
+class CartView(LoginRequiredMixin, ListView):
+    model = Transaction
+    template_name = 'cart.html'
+    context_object_name = 'transactions'
+
+    def get_queryset(self):
+        return Transaction.objects.filter(buyer=self.request.user)
+
+class TransactionListView(LoginRequiredMixin, ListView):
+    model = Transaction
+    template_name = 'transaction_list.html'
+    context_object_name = 'transactions'
+
+    def get_queryset(self):
+        return Transaction.objects.filter(product__owner=self.request.user)
