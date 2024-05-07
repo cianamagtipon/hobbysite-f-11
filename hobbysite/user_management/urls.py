86519d7c4b16aa1e7a0c9cbc5671429be1cfd2cd
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-08 00:13:42 +0800

    Added user profile

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import index, UpdateView
-from django.views.generic.edit import UpdateView
+from .views import UpdateView
 
 urlpatterns=[
-    path('', index, name='index'),
     path('profile', UpdateView.as_view(), name='profile'),
 ]
 
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,3 +1,14 @@
+from .models import Profile
+from .forms import ProfileForm
 from django.shortcuts import render
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class UpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = "profile.html"
+    
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
