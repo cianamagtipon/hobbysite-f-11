fa0de0df86eb2930c833b60be1fa611bbfe83e53
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-07 13:15:43 +0800

    Forum: updated forms and added url.

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -3,8 +3,8 @@ from .views import index, ThreadListView, ThreadDetailView, ThreadCreateView
 
 urlpatterns=[
     path('', index, name='index'),
+    path('thread/add', ThreadCreateView.as_view()),
     path('threads/', ThreadListView.as_view(), name='threads'),
-    path('create/', ThreadCreateView.as_view()),
     path('thread/<int:pk>', ThreadDetailView.as_view(), name='thread-detail')
 ]
 
