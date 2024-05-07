cf6740d1b19e4731ec306f2f31a898480bcf0b12
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-07 11:31:24 +0800

    Forum: added ThreadUpdate and ThreadCreate in views

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from hobbysite.forum.forms import ThreadForm
 from .models import Thread
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 def index(request):
     return HttpResponse('Welcome to Forum Threads!')
@@ -17,3 +22,18 @@ class ThreadListView(ListView):
 class ThreadDetailView(DetailView):
     model = Thread
     template_name = 'thread_detail.html'
+    
+
+class ThreadCreateView(CreateView):
+    model = Thread
+    form_class = ThreadForm
+    template_name = "task_create.html"
+    
+    def get_success_url(self):
+        return reverse_lazy("tasks:list")
+    
+
+class ThreadUpdateView(LoginRequiredMixin, UpdateView):
+    model = Thread
+    form_class = ThreadForm
+    template_name = "task_detail.html"
