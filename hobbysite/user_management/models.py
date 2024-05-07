ba2729826639d081122aa50f3b800735b88b63aa
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-07 22:17:59 +0800

    modified profile according to specs

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    short_bio = models.TextField(validators=[MinLengthValidator(255)])
+    name = models.CharField(max_length=63)
+    email_address = models.EmailField()
