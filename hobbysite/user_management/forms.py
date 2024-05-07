b73df991068fa7ca22ec5be27fd0ec8f0bd74c16
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-08 00:55:11 +0800

    Updated forms

--- hobbysite/user_management/forms.py
+++ hobbysite/user_management/forms.py
@@ -5,8 +5,3 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = "__all__"
-        widgets = {
-            "due_date":forms.TextInput(
-                attrs={"type": "datetime-local"}
-            )
-        }
