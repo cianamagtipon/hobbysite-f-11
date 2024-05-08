1accaaafb401370de8f3532981e63e4784397eef
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-05-08 22:00:25 +0800

    fixed urlpatterns in urls.py

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -5,7 +5,7 @@ app_name = 'wiki'
 
 urlpatterns = [
     path('articles/', views.article_list_view, name='article-list'),
-    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
-    path('article/add/', views.ArticleCreateView.as_view(), name='article-create'),
-    path('article/<int:pk>/edit/', views.ArticleUpdateView.as_view(), name='article-update'),
+    path('article/<int:pk>/', views.article_detail_view, name='article-detail'),
+    path('article/create/', views.article_create_view, name='article-create'),
+    path('article/<int:pk>/update/', views.article_update_view, name='article-update'),
 ]
