8dd9eb9342383371776590f2788d601b2feff9d2
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-05-08 14:10:46 +0800

    updated and added models.py

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -7,7 +8,6 @@ class ArticleCategory(models.Model):
     def __str__ (self):
         return self.name
     
-
     class Meta:
         ordering = ['name']
         verbose_name = "Article Category"
@@ -16,16 +16,28 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True)
     entry = models.TextField()
+    # header_image = models.ImageField(upload_to='article_images')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
     def __str__ (self):
         return self.title
     
-
     class Meta:
         ordering = ['-created_on']
         verbose_name = "Article"
         verbose_name_plural = "Articles"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
