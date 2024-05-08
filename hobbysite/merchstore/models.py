94c4fbc10c518371e81879bcae0e473af31da08d
ClydeGerance <clyde.gerance@student.ateneo.edu>
2024-05-08 16:00:54 +0800

    removed local profile

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='merchstore_profile')
-    display_name = models.CharField(max_length=63)
-    email = models.EmailField()
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='merchstore_profile')
+#     display_name = models.CharField(max_length=63)
+#     email = models.EmailField()
 
-    def __str__(self):
-        return self.display_name
+#     def __str__(self):
+#         return self.display_name
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
