49231a86d989ae35204b884e48e22842bfdc3d1c
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-12 22:12:24 +0800

    merged forum and wiki

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -9,7 +9,9 @@ urlpatterns=[
     path('forum/', include('forum.urls', namespace="forum")),
     path('wiki/', include('wiki.urls', namespace="wiki")),
     path('merchstore/', include('merchstore.urls', namespace="merchstore")),
-    path('commissions/', include('commissions.urls', namespace="commissions"))
+    path('commissions/', include('commissions.urls', namespace="commissions")),
+    path('accounts/', include('django.contrib.auth.urls'))
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
