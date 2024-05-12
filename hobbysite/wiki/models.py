c6bbf3ab21a5879cda3b48a47a3d309185cf8ea5
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-05-12 23:31:27 +0800

    restructured models.py

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,43 +1,87 @@
+
 from django.db import models
-from django.contrib.auth.models import User
+from django.urls import reverse
+from user_management.models import Profile
+
+from time import timezone
+import datetime
+
 
 class ArticleCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=225)
     description = models.TextField()
     
-    def __str__ (self):
+    def __str__(self):
         return self.name
     
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
         verbose_name = "Article Category"
         verbose_name_plural = "Article Categories"
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=225)
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    article_image = models.ImageField(upload_to ='article_images/', null=True)
+    author = models.ForeignKey(
+        Profile, 
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=True
+    )
+    category = models.ForeignKey(
+        ArticleCategory, 
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=True,
+        related_name='articles'
+    )
     
-    def __str__ (self):
+    def __str__(self):
         return self.title
     
+    def get_root_url(self):
+        return reverse('wiki:articles')
+
+    def get_absolute_url(self):
+        return reverse('wiki:article-detail', args=[self.pk])
+    
+    
     class Meta:
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
         verbose_name = "Article"
         verbose_name_plural = "Articles"
-
-
+        
+        
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        Profile, 
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=True
+    )
+    article = models.ForeignKey(
+        Article, 
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
 
+    def __str__(self):
+        return self.entry
+    
+    def get_root_url(self):
+        return reverse('wiki:articles')
+
+    def get_absolute_url(self):
+        return reverse('wiki:article-detail', args=[self.pk])
+    
+    
     class Meta:
-        ordering = ['created_on']
+        ordering = ["created_on"]
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
