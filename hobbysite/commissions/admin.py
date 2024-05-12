cb2a0be4c6d2f397fc044317fa240b024ad5afb9
kanishajison <kanisha.jison@student.ateneo.edu>
2024-05-13 00:24:39 +0800

    Update admin

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from .models import Commission, Job, JobApplication
 
+
 @admin.register(Commission)
 class CommissionAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'created_on', 'updated_on']
     list_filter = ['created_on', 'updated_on']
     search_fields = ['title', 'description']
 
+
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ['commission', 'role', 'manpower_required', 'status']
     list_filter = ['status', 'role']
     search_fields = ['role', 'commission__title']
+
 
 @admin.register(JobApplication)
 class JobApplicationAdmin(admin.ModelAdmin):
