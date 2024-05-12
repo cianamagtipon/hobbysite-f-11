fdfea499104632a675930877d8d44f429804a61d
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-05-12 18:46:53 +0800

    fixed urls

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -3,10 +3,10 @@ from . import views
 from .views import ArticleForm
 
 urlpatterns = [
-    path('articles/', views.ArticleView.as_view(), name='article-list'),
-    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
-    path('article/create/', views.ArticleCreateView.as_view(), name='article-create'),
-    path('article/<int:pk>/update/', views.ArticleUpdateView.as_view(), name='article-update'),
+    path('articles/', views.article_list_view, name='article-list'),
+    path('article/<int:pk>/', views.article_detail_view, name='article-detail'),
+    path('article/create/', views.article_create_view, name='article-create'),
+    path('article/<int:pk>/update/', views.article_update_view, name='article-update'),
 ]
 
 app_name = 'wiki'
