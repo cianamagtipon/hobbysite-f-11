3fe3475180b842926a595b9bac71d9ea59de2a66
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-05-12 23:29:16 +0800

    Updated admin.py

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,21 +1,37 @@
+
 from django.contrib import admin
 from .models import Article, ArticleCategory, Comment
 
+
 class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+    
     search_fields = ["name"]
-    list_display = ["name", "description"]
-    fieldsets = [("Details", {"fields": ["name", "description"]})]
+    list_display = ["name"]
+    
+    fieldsets = [("Details", {"fields": ("name", "description")})]
+
 
 class ArticleAdmin(admin.ModelAdmin):
-    search_fields = ["title"]
-    list_display = ["title", "author", "category", "created_on", "updated_on"]
-    list_filter = ["category", "created_on", "updated_on"]
-    fieldsets = [("Details", {"fields": ["title", "author", "category", "entry"]})]
+    model = Article
+
+    search_fields = ["title", "author"]
+    list_display = ["title", "category", "created_on", "updated_on"]
+    list_filter = ["created_on", "updated_on"]
+
+    fieldsets = [
+        ("Details", {"fields": [("title", "entry"), "author", "category"]})]
+
 
 class CommentAdmin(admin.ModelAdmin):
-    search_fields = ["entry"]
-    list_display = ["entry", "author", "article", "created_on", "updated_on"]
+    model = Comment
+
+    list_display = ["entry", "created_on", "updated_on"]
     list_filter = ["created_on", "updated_on"]
+
+    fieldsets = [
+        ("Details", {"fields": [("entry"), "article", "author"]})]
+
 
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
