452ec348d08b940de878f7ae7224b0d8705321f5
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-05-12 23:29:59 +0800

    fixed forms.py

--- hobbysite/wiki/forms.py
+++ hobbysite/wiki/forms.py
@@ -1,12 +1,18 @@
-from django import forms
-from .models import Comment, Article
+from django import forms 
+from .models import Article, Comment
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'author', 'category', 'entry']
+        fields = ["title", "entry", "category"]
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["entry"]
+        widgets = {
+            "entry": forms.Textarea(
+                attrs={"cols": 80, "rows": 10}
+            )
+        }
