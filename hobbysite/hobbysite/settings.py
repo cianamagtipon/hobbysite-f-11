960b1f438b39a200bcdd49d8d9a1fc9658f1bb35
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-05-12 14:10:59 +0800

    Updated settings.py for image upload

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -132,10 +132,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+BASE_DIR/'static',
+]
 
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = (BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
