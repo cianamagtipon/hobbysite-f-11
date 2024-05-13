532da0ebee560abb909057b24cc91e4373927e6a
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-13 12:14:17 +0800

    Commission: updated models

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.urls import reverse
 
-class Profile(AbstractUser):
-    display_name = models.CharField(max_length=63, unique=True)
-    email = models.EmailField(unique=True)  # Use the built-in email field and ensure it's unique
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=63)
+    email_address = models.EmailField()
+        
     def __str__(self):
         return self.display_name
-
+    
     def get_absolute_url(self):
-        return reverse('user_management:update_profile')  # Update the URL name based on your URL configuration
+        return reverse('user_management:profile')
