2b3e1a7f75a3575e4ae05b8b28dbb21c77b468c3
ciana louisse <ciana.magtipon@student.ateneo.edu>
2024-05-13 12:11:05 +0800

    Commission: modified forms

--- hobbysite/user_management/forms.py
+++ hobbysite/user_management/forms.py
@@ -5,7 +5,7 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['display_name', 'email']  # Specify fields to be included in the form
+        fields = "__all__" # Specify fields to be included in the form
 
 class SignUpForm(UserCreationForm):
     class Meta:
