fa3747226595ec0b6f0c1313e4da482b99ec21cf
kanishajison <kanisha.jison@student.ateneo.edu>
2024-05-14 12:23:58 +0800

    Update forums

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -1,11 +1,12 @@
+# forum/urls.py
 from django.urls import path
 from .views import index, ThreadListView, ThreadDetailView, ThreadCreateView, ThreadUpdateView
 
 app_name = 'forum'
 
-urlpatterns = [ 
-    path('threads/', ThreadListView.as_view(), name='threads'),  # List view
-    path('thread/add', ThreadCreateView.as_view(), name='thread-add'),  # Create view
-    path('thread/<int:pk>/', ThreadDetailView.as_view(), name='thread-detail'),  # Detail view
-    path('thread/<int:pk>/edit', ThreadUpdateView.as_view(), name='thread-edit'),  # Edit view
+urlpatterns = [
+    path('', ThreadListView.as_view(), name='threads'),  # Renamed from index to threads
+    path('thread/add', ThreadCreateView.as_view(), name='thread-add'),
+    path('thread/<int:pk>/edit', ThreadUpdateView.as_view(), name='thread-edit'),
+    path('thread/<int:pk>', ThreadDetailView.as_view(), name='thread-detail')
 ]
